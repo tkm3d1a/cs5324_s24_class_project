f0ae65b0eeedfd1acee2bc86834dfd4b0c33d852
Tim Klimpel <tim.klimpel@gmail.com>
2024-01-11 23:03:23 -0500

    Added Nuxtjs project Structure to /source_code/frontend

---
+++ source_code/frontend/cs5324-frontend/app.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>
+    Hello World!!
+  </h1>
+  <p>
+    Simple test paragraph - trying to get CI up and running possibly
+  </p>
+  <!-- <div>
+    <NuxtWelcome />
+  </div> -->
+</template>
