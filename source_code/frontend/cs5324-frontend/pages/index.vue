43ac7a8e8fce51563a3e05608bc5d2eac7547ea6
Tim Klimpel <tim.klimpel@gmail.com>
2024-01-28 18:51:41 -0500

    Initial tiptap install and styling

--- source_code/frontend/cs5324-frontend/pages/index.vue
+++ source_code/frontend/cs5324-frontend/pages/index.vue
@@ -1,6 +1,28 @@
 <template>
-	<div>
-		<h1>Welcome to the Index Page</h1>
-		<p>This is the main page of your application.</p>
-	</div>
+	<header class="border-4 m-1">
+		<nav>
+			<ul>
+				<li class="border-2 m-1 p-px">
+					<NuxtLink to="/">Home</NuxtLink>
+				</li>
+				<li class="border-2 m-1 p-px">
+					<NuxtLink to="/about">About</NuxtLink>
+				</li>
+				<li class="border-2 m-1 p-px">
+					<NuxtLink to="/editor">Editor</NuxtLink>
+				</li>
+			</ul>
+		</nav>
+	</header>
 </template>
+  
+<script>
+import Tiptap from '../components/Tiptap.vue'
+
+export default {
+	name: 'App',
+	components: {
+		Tiptap
+	}
+}
+</script>
