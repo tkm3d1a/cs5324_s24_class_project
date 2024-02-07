ba7a59370e60538c731e7fe7327e43e3aae29281
MattHays2 <mhays@distorted.dev>
2024-02-07 12:30:36 -0700

    Added the Post Entity

---
+++ source_code/backend/CS_department_monitor_backend/src/main/java/com/cs5324/monitorbackend/entity/Post.java
@@ -0,0 +1,49 @@
+package com.cs5324.monitorbackend.entity;
+
+import com.cs5324.monitorbackend.entity.enums.ItemStatus;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Data
+@NoArgsConstructor(access = AccessLevel.PROTECTED, force = true)
+@AllArgsConstructor
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @Version
+    private Long version;
+
+    @NotBlank
+    @Size(min = 5, message = "Title must be at least 5 characters long")
+    @Column(nullable = false)
+    private String title;
+
+    @NotBlank
+    @Size(min = 5, message = "Content must be at least 5 characters long")
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    @NotNull
+    @Column(nullable = false)
+    private ItemStatus status = ItemStatus.PENDING;
+
+    @NotNull
+    @Column(nullable = false)
+    private Boolean isStagedForDisplay = false;
+}
---
+++ source_code/backend/CS_department_monitor_backend/src/main/java/com/cs5324/monitorbackend/entity/enums/ItemStatus.java
@@ -0,0 +1,7 @@
+package com.cs5324.monitorbackend.entity.enums;
+
+public enum ItemStatus {
+    PENDING,
+    APPROVED,
+    REJECTED
+}
