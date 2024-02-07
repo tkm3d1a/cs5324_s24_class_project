a1270b3b38083ff606e16d9e847a6bbc69942726
MattHays2 <mhays@distorted.dev>
2024-02-07 12:44:44 -0700

    Added Notification entity

---
+++ source_code/backend/CS_department_monitor_backend/src/main/java/com/cs5324/monitorbackend/entity/Notification.java
@@ -0,0 +1,27 @@
+package com.cs5324.monitorbackend.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Data
+@NoArgsConstructor(access = AccessLevel.PROTECTED, force = true)
+@AllArgsConstructor
+public class Notification {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @Version
+    private Long version;
+}
