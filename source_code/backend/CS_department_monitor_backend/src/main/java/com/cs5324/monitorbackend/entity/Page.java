90480c22e706e7fa0f3a238db88f8e2ef3b349c3
MattHays2 <mhays@distorted.dev>
2024-02-13 18:45:45 -0700

    Added Page

---
+++ source_code/backend/CS_department_monitor_backend/src/main/java/com/cs5324/monitorbackend/entity/Page.java
@@ -0,0 +1,34 @@
+package com.cs5324.monitorbackend.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Data
+@NoArgsConstructor(access = AccessLevel.PROTECTED, force = true)
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class Page {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @NotBlank
+    @Column(nullable = false)
+    private final String title;
+
+    private final String content;
+
+    @NotNull
+    @Column(nullable = false)
+    private final Boolean isTagged = false;
+
+    @NotNull
+    @OneToOne
+    @Column(nullable = false)
+    private Event event;
+}
