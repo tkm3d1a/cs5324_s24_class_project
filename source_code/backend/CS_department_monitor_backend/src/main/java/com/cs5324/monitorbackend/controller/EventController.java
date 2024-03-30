2067b3e2efb0857f4f47e15b8b0da083674a8230
ssfaron <slsmallw.asu@gmail.com>
2024-03-30 00:25:42 -0500

    implement endpoints for UC05

--- source_code/backend/CS_department_monitor_backend/src/main/java/com/cs5324/monitorbackend/controller/EventController.java
+++ source_code/backend/CS_department_monitor_backend/src/main/java/com/cs5324/monitorbackend/controller/EventController.java
@@ -1,13 +1,44 @@
 package com.cs5324.monitorbackend.controller;
 
+import com.cs5324.monitorbackend.entity.Event;
+import com.cs5324.monitorbackend.entity.Notification;
+import com.cs5324.monitorbackend.entity.Page;
 import com.cs5324.monitorbackend.service.EventService;
+import com.cs5324.monitorbackend.repository.EventRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.util.Set;
+import java.util.UUID;
 
 @RestController
 @RequestMapping(path = "/api/events", produces = "application/json")
 @RequiredArgsConstructor
 public class EventController {
     private final EventService eventService;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity getOwnedEvents(UUID userId){
+        Set<Event> ownedEvents = eventService.getEvents(userId);
+        return new ResponseEntity(ownedEvents, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/selected", method = RequestMethod.GET)
+    public ResponseEntity getSelectedEvent(Set<Event> ownedEvents, UUID eventId){
+        Event selectedEvent = eventService.getEvent(ownedEvents, eventId);
+        return new ResponseEntity(selectedEvent, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/selected/edit", method = RequestMethod.POST)
+    public ResponseEntity submitEditedEvent(Event editedEvent){
+        Notification editedNotif = eventService.submitEdits(editedEvent);
+        return new ResponseEntity(editedNotif, HttpStatus.OK);
+    }
+
 }
