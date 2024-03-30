f07b735ee0dca7199ff1021903baa75c7e2eeff9
ssfaron <slsmallw.asu@gmail.com>
2024-03-30 00:25:42 -0500

    implement endpoints for UC05

--- source_code/backend/CS_department_monitor_backend/src/main/java/com/cs5324/monitorbackend/repository/EventRepository.java
+++ source_code/backend/CS_department_monitor_backend/src/main/java/com/cs5324/monitorbackend/repository/EventRepository.java
@@ -1,12 +1,16 @@
 package com.cs5324.monitorbackend.repository;
 
 import com.cs5324.monitorbackend.entity.Event;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface EventRepository extends JpaRepository<Event, UUID>{
+
 
 }
--- source_code/backend/CS_department_monitor_backend/src/main/java/com/cs5324/monitorbackend/service/EventService.java
+++ source_code/backend/CS_department_monitor_backend/src/main/java/com/cs5324/monitorbackend/service/EventService.java
@@ -1,10 +1,20 @@
 package com.cs5324.monitorbackend.service;
 
+import com.cs5324.monitorbackend.entity.Event;
+import com.cs5324.monitorbackend.entity.Notification;
+import com.cs5324.monitorbackend.entity.Page;
+import com.cs5324.monitorbackend.entity.User;
+import com.cs5324.monitorbackend.entity.enums.ItemStatus;
 import com.cs5324.monitorbackend.repository.EventRepository;
+import com.cs5324.monitorbackend.repository.UserRepository;
 import jakarta.annotation.Resource;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.Set;
+import java.util.Optional;
+import java.util.UUID;
 
 @Slf4j
 @Service
@@ -12,4 +22,38 @@ import org.springframework.stereotype.Service;
 public class EventService{
     @Resource
     private final EventRepository eventRepo;
+
+    @Resource
+    private final UserRepository userRepo;
+
+    public Set<Event> getEvents(UUID userId){
+
+        Optional<User> user = userRepo.findById(userId);
+        Set<Event> ownedEvents = user.get().getEvents();
+
+        return ownedEvents;
+    }
+
+    public Event getEvent(Set<Event> ownedEvents, UUID eventId){
+
+        for(Event event : ownedEvents){
+            if(event.getId() == eventId){
+                return event;
+            }
+        }
+
+        return null;
+    }
+
+    public Notification submitEdits(Event editedEvent){
+        editedEvent.setApprovalStatus(ItemStatus.PENDING);
+        editedEvent.setDateOfEvent(editedEvent.getDateOfEvent());
+        editedEvent.setPage(editedEvent.getPage());
+
+        Notification editedNotif = new Notification();
+        editedEvent.setNotification(editedNotif);
+
+        return editedNotif;
+    }
+
 }
