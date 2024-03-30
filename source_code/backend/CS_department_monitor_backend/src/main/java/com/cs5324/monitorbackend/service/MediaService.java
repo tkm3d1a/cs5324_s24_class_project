12684eff3e80e2a36171b57c438e453ad394e87d
Tim Klimpel <tim.klimpel@gmail.com>
2024-03-30 13:54:48 -0400

    Update media endpoint added

--- source_code/backend/CS_department_monitor_backend/src/main/java/com/cs5324/monitorbackend/service/MediaService.java
+++ source_code/backend/CS_department_monitor_backend/src/main/java/com/cs5324/monitorbackend/service/MediaService.java
@@ -2,8 +2,10 @@ package com.cs5324.monitorbackend.service;
 
 import com.cs5324.monitorbackend.entity.Media;
 import com.cs5324.monitorbackend.entity.MediaDTO;
+import com.cs5324.monitorbackend.entity.enums.ItemStatus;
 import com.cs5324.monitorbackend.entity.enums.MediaType;
 import com.cs5324.monitorbackend.exception.InvalidMediaTypeException;
+import com.cs5324.monitorbackend.exception.MediaDoesNotExistException;
 import com.cs5324.monitorbackend.repository.MediaRepository;
 import jakarta.annotation.Resource;
 import lombok.RequiredArgsConstructor;
@@ -12,6 +14,8 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Slf4j
 @Service
@@ -37,6 +41,22 @@ public class MediaService{
         return mediaRepo.findAll(Sort.by(Sort.Direction.DESC, "createdAt"));
     }
     //UPDATE
+    public Media updateMedia(UUID mediaId, MediaDTO mediaDTO){
+        log.info("Updating existing media entry");
+        Optional<Media> mediaToUpdateOpt = mediaRepo.findById(mediaId);
+        if(mediaToUpdateOpt.isEmpty()){
+            log.warn("Media not found");
+            throw new MediaDoesNotExistException();
+        }
+        Media mediaToUpdate = mediaToUpdateOpt.get();
+        mediaToUpdate.setTitle(mediaDTO.getTitle());
+        mediaToUpdate.setLink(mediaDTO.getLink());
+        if(mediaToUpdate.getItemStatus() != ItemStatus.PENDING){
+            mediaToUpdate.setItemStatus(ItemStatus.PENDING);
+            mediaToUpdate.setIsTagged(false);
+        }
+        return mediaRepo.save(mediaToUpdate);
+    }
     //DELETE
 
     //PROTECTED
