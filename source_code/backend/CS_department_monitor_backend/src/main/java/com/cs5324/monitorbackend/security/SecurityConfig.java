17210f57f60f7c1791d9afd0f3c783180e13b31e
Tim Klimpel <tim.klimpel@gmail.com>
2024-03-30 13:11:56 -0400

    Updated SecurityConfig for api requests and basic auth

--- source_code/backend/CS_department_monitor_backend/src/main/java/com/cs5324/monitorbackend/security/SecurityConfig.java
+++ source_code/backend/CS_department_monitor_backend/src/main/java/com/cs5324/monitorbackend/security/SecurityConfig.java
@@ -2,12 +2,16 @@ package com.cs5324.monitorbackend.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -19,16 +23,27 @@ public class SecurityConfig {
     }
 
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.formLogin(Customizer.withDefaults());
-        httpSecurity.csrf().disable(); //May be old item - marked as deprecated
-        httpSecurity.headers().frameOptions().disable(); //May be old item - marked as deprecated
-        httpSecurity
-                .authorizeHttpRequests() //May be old item - marked as deprecated
-                .requestMatchers("/test").hasAuthority("ADMIN")
-                .requestMatchers("/").permitAll()
-                .anyRequest().authenticated();
-        return httpSecurity.build();
+    @Order(1)
+    SecurityFilterChain apiSecurityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .securityMatcher("/api/**")
+                .authorizeHttpRequests(auth -> {
+                    auth.anyRequest().authenticated();
+                })
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .httpBasic(withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .build();
     }
+
+//    @Bean
+//    @Order(0)
+//    SecurityFilterChain securityFilterChainBase(HttpSecurity httpSecurity) throws Exception {
+//        httpSecurity.formLogin(Customizer.withDefaults());
+//        httpSecurity
+//                .authorizeHttpRequests() //May be old item - marked as deprecated
+//                .requestMatchers("/").permitAll();
+//        return httpSecurity.build();
+//    }
 
 }
