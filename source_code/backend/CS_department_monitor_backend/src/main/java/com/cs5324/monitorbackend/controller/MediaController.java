ed06bfafd3de1e927587f0355d4a609071859cb5
Tim Klimpel <tim.klimpel@gmail.com>
2024-03-30 13:12:58 -0400

    Basic GET all and POST single created for Media

--- source_code/backend/CS_department_monitor_backend/src/main/java/com/cs5324/monitorbackend/controller/MediaController.java
+++ source_code/backend/CS_department_monitor_backend/src/main/java/com/cs5324/monitorbackend/controller/MediaController.java
@@ -1,13 +1,36 @@
 package com.cs5324.monitorbackend.controller;
 
+import com.cs5324.monitorbackend.entity.MediaDTO;
 import com.cs5324.monitorbackend.service.MediaService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@Slf4j
 @RestController
 @RequestMapping(path = "/api/media", produces = "application/json")
 @RequiredArgsConstructor
 public class MediaController {
     private final MediaService mediaService;
+
+    @GetMapping
+    public ResponseEntity<?> getAllMediaDefault(){
+        Map<String,Object> response = new LinkedHashMap<>();
+        response.put("count",mediaService.getAllMediaSortByCreatedDesc().size());
+        response.put("media",mediaService.getAllMediaSortByCreatedDesc());
+        return ResponseEntity.ok().body(response);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createNewMedia(@RequestBody MediaDTO mediaDTO){
+        log.info("MediaDTO (controller): {}", mediaDTO);
+        Map<String,Object> response = new LinkedHashMap<>();
+        response.put("result", "successful");
+        response.put("savedMedia", mediaService.createNewMediaEntry(mediaDTO));
+        return ResponseEntity.ok().body(response);
+    }
 }
--- source_code/backend/CS_department_monitor_backend/src/main/java/com/cs5324/monitorbackend/service/MediaService.java
+++ source_code/backend/CS_department_monitor_backend/src/main/java/com/cs5324/monitorbackend/service/MediaService.java
@@ -1,15 +1,44 @@
 package com.cs5324.monitorbackend.service;
 
+import com.cs5324.monitorbackend.entity.Media;
+import com.cs5324.monitorbackend.entity.MediaDTO;
+import com.cs5324.monitorbackend.entity.enums.MediaType;
+import com.cs5324.monitorbackend.exception.InvalidMediaTypeException;
 import com.cs5324.monitorbackend.repository.MediaRepository;
 import jakarta.annotation.Resource;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class MediaService{
-    @Resource
-    private final MediaRepository mediaRepo;
+    @Resource private final MediaRepository mediaRepo;
+
+    //CREATE
+    public Media createNewMediaEntry(MediaDTO mediaDTO){
+        log.info("MediaDTO: {}", mediaDTO);
+        Media newMedia = new Media();
+        newMedia.setTitle(mediaDTO.getTitle());
+        newMedia.setLink(mediaDTO.getLink());
+        switch(mediaDTO.getMediaType().toLowerCase()){
+            case "picture","pic","image" -> newMedia.setMediaType(MediaType.IMAGE);
+            case "vid","video","movie" -> newMedia.setMediaType(MediaType.VIDEO);
+            default -> throw new InvalidMediaTypeException();
+        }
+        return mediaRepo.save(newMedia);
+    }
+    //READ
+    public List<Media> getAllMediaSortByCreatedDesc(){
+        return mediaRepo.findAll(Sort.by(Sort.Direction.DESC, "createdAt"));
+    }
+    //UPDATE
+    //DELETE
+
+    //PROTECTED
+    //PRIVATE
 }
