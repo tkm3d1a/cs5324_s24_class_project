e494ead0e32349e7d6d28f4a31650b03e0b0e04a
Matt-Hays <mkhays01@gmail.com>
2024-04-07 19:34:28 -0600

    Add Review for Approval

--- source_code/backend/CS_department_monitor_backend/src/main/java/com/cs5324/monitorbackend/controller/ReviewController.java
+++ source_code/backend/CS_department_monitor_backend/src/main/java/com/cs5324/monitorbackend/controller/ReviewController.java
@@ -1,13 +1,28 @@
 package com.cs5324.monitorbackend.controller;
 
+import com.cs5324.monitorbackend.entity.Notification;
 import com.cs5324.monitorbackend.service.ReviewService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/api/reviews", produces = "application/json")
 @RequiredArgsConstructor
 public class ReviewController {
     private final ReviewService reviewService;
+
+    @GetMapping
+    public ResponseEntity<Iterable<Notification>> getAllNotifications() {
+        try {
+            return ResponseEntity.ok(reviewService.getAllNotifications());
+        } catch (Exception ex) {
+            return ResponseEntity.internalServerError().body(null);
+        }
+    }
+
+    @PostMapping(consumes = "application/json")
+    public ResponseEntity<Notification> reviewForApproval(@RequestBody Notification notification) {
+        return ResponseEntity.ok(reviewService.reviewForApproval(notification));
+    }
 }
