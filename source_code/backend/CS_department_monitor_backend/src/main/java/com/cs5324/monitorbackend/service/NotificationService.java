39edc77a7b13b97297a2639c86f677087eb19c8f
Matt-Hays <mkhays01@gmail.com>
2024-04-07 19:34:28 -0600

    Add Review for Approval

--- source_code/backend/CS_department_monitor_backend/src/main/java/com/cs5324/monitorbackend/service/NotificationService.java
+++ source_code/backend/CS_department_monitor_backend/src/main/java/com/cs5324/monitorbackend/service/NotificationService.java
@@ -1,15 +1,23 @@
 package com.cs5324.monitorbackend.service;
 
+import com.cs5324.monitorbackend.entity.Notification;
 import com.cs5324.monitorbackend.repository.NotificationRepository;
-import jakarta.annotation.Resource;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class NotificationService{
-    @Resource
     private final NotificationRepository notificationRepo;
+
+    public Iterable<Notification> getNotifications() {
+        return notificationRepo.findAll(Sort.by(Sort.Direction.DESC, "createdAt"));
+    }
+
+    public Notification saveNotification(Notification notification) {
+        return notificationRepo.save(notification);
+    }
 }
