71c70401ffbd2fce51dd10883c95b3fc463f45dd
Tim Klimpel <tim.klimpel@gmail.com>
2024-04-10 20:19:47 -0400

    Partial update for exposing DisplayController

--- source_code/backend/CS_department_monitor_backend/src/main/java/com/cs5324/monitorbackend/controller/DisplayController.java
+++ source_code/backend/CS_department_monitor_backend/src/main/java/com/cs5324/monitorbackend/controller/DisplayController.java
@@ -2,12 +2,25 @@ package com.cs5324.monitorbackend.controller;
 
 import com.cs5324.monitorbackend.service.DisplayService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping(path = "/api/display", produces = "application/json")
 @RequiredArgsConstructor
 public class DisplayController {
     private final DisplayService displayService;
+
+    @GetMapping("/media")
+    public ResponseEntity<?> getMediaToDisplay(){
+        Map<String,Object> response = new LinkedHashMap<>();
+        response.put("count", displayService.getMediaToDisplay().size());
+        response.put("media", displayService.getMediaToDisplay());
+        return ResponseEntity.ok().body(response);
+    }
 }
